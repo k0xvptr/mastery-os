1408e9def0ed5d6a8ee1743ea303af1e9f65a485
RMSH0410 <ramushu0410@gmail.com>
2026-05-09 21:01:01 -0400

    SO here's the ai in python
    There's a tools doc if you want to give ai access to anywhere but I don't think you will need it.
    
    Anyways it is what it is

---
+++ ai.py
@@ -0,0 +1,46 @@
+from pydantic_ai.models.google import GoogleModel
+from dotenv import load_dotenv
+from pydantic_ai import Agent
+import tools
+load_dotenv()
+model = GoogleModel("gemini-3-flash-preview")
+
+q_agent = Agent(model,
+              system_prompt="generates a question based on data I give you (subject, uploaded files, JSON)",
+              tools=[])
+f_agent = Agent(model,
+              system_prompt="given you data on what the user did bad, returnss feedback on how to improve",
+              tools=[])
+
+a_agent = Agent(model,
+              system_prompt="given you the question, returns a solution with explanations",
+              tools=[])
+
+
+def question_agent(datas):
+    #question_generation_agent
+    user_input = str(datas)
+    try:
+        resp = q_agent.run_sync(user_input)
+        return resp.output
+    except Exception as e:
+        return f"Error occurred: {e}"
+
+
+def feedback_agent(datas):
+    user_input = str(datas)
+    try:
+        resp = f_agent.run_sync(user_input)
+        return resp.output
+    except Exception as e:
+        return f"Error occurred: {e}"
+
+
+def solution_agent(datas):
+    user_input = str(datas)
+    try:
+        resp = a_agent.run_sync(user_input)
+        return resp.output
+    except Exception as e:
+        return f"Error occurred: {e}"
+
